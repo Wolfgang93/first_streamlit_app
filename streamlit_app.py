331a5c1fdf3de0477284a28b866b8dca8ccff9f9
Wolfgang93 <102249519+Wolfgang93@users.noreply.github.com>
2023-07-05 12:50:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.header('🥗breakfast menu')
 streamlit.text('🐔 omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞Hard-Boiled Free Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
